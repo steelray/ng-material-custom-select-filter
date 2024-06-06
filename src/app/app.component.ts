d908d1b53098458137bfde3471a2b8a2f1a3786f
Bolat <nrka99@gmail.com>
2024-06-06 11:17:59 +0700

    select component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,86 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { IOption, SelectComponent } from './select/select.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    SelectComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  title = 'material-custom-select-filter';
+export class AppComponent implements OnInit {
+  customers: IOption[] = [
+    {
+      value: 1,
+      title: 'Vi'
+    },
+    {
+      value: 2,
+      title: 'Pari'
+    },
+    {
+      value: 3,
+      title: 'John'
+    },
+    {
+      value: 4,
+      title: 'Juna'
+    },
+    {
+      value: 5,
+      title: 'Rafik'
+    },
+    {
+      value: 6,
+      title: 'Dinara'
+    },
+    {
+      value: 7,
+      title: 'Sveta'
+    },
+    {
+      value: 8,
+      title: 'Mai'
+    },
+    {
+      value: 9,
+      title: 'Puti'
+    },
+    {
+      value: 10,
+      title: 'Pao'
+    },
+  ];
+
+  ngOnInit(): void {
+    // for testing, if the filter input disappears if the length is <= 5
+    setTimeout(() => {
+      this.customers = [
+        {
+          value: 4,
+          title: 'Juna'
+        },
+        {
+          value: 7,
+          title: 'Sveta'
+        },
+        {
+          value: 8,
+          title: 'Mai'
+        },
+        {
+          value: 9,
+          title: 'Puti'
+        },
+        {
+          value: 10,
+          title: 'Pao'
+        },
+      ];
+    }, 5000);
+  }
 }
---
+++ src/app/select/select.component.ts
@@ -0,0 +1,48 @@
+import { NgIf } from '@angular/common';
+import { ChangeDetectionStrategy, Component, Input, OnChanges, SimpleChanges } from '@angular/core';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
+import {MatSelectModule} from '@angular/material/select';
+
+export interface IOption {
+  value: any;
+  title: string;
+}
+
+@Component({
+  selector: 'app-select',
+  standalone: true,
+  imports: [
+    NgIf,
+    MatSelectModule,
+    ReactiveFormsModule
+  ],
+  templateUrl: './select.component.html',
+  styleUrl: './select.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class SelectComponent implements OnChanges{
+  @Input() options: IOption[] = [];
+  @Input() label!: string | undefined;
+  @Input() control: FormControl = new FormControl();
+  @Input() showFilterInputLength = 10; // if more than input value show filter input
+  @Input() hideFilterInput = false; // force hiding filter input event if options length more than given "showFilterInputLength"
+  @Input() filterInputPlaceholder: string = 'search...';
+
+  filteredOptions: IOption[] = [];
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if(changes['options']?.currentValue) {
+      this.filteredOptions = changes['options'].currentValue; // always reset filtered list whene getting new options 
+    }
+  }
+
+  onFilter(event: KeyboardEvent): void {
+    const target = event.target;
+    if(target instanceof HTMLInputElement) {
+      const options = [...this.options]; // clone to avoid affect the origin options
+      const value = target.value?.trim().toLocaleLowerCase(); // transforming to avoid case sensitive
+      this.filteredOptions = value ? options.filter(option => option.title.trim().toLocaleLowerCase().includes(value)) : this.options;
+    } 
+  }
+
+}
